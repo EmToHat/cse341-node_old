b44719211002b279956fd3aced4dbc18055cff9e
Emilee Hatch <121782677+EmToHat@users.noreply.github.com>
2023-09-20 16:57:40 -0600

    Delete week01/01Personal directory

--- week01/01Personal/routes/index.js
+++
@@ -1,8 +0,0 @@
-// express is a web framework for Node.js
-const indexRoute = require('express').Router();
-
-const indexController = require('../controllers');
-
-indexRoute.get('/', indexController.getFullName);
-
-module.exports = indexRoute;
