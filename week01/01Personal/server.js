1ea06e1b92d8fbfac92f17663f60cd51c43ad67c
Emilee Hatch <121782677+EmToHat@users.noreply.github.com>
2023-09-21 19:38:32 -0600

    Delete week01/01Personal directory

--- week01/01Personal/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use('/', require('./routes'))
-
-app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
